5c8043e3a3fdf0425f8d75a8229c7d83475c2683
Mahesh Kakde <maheshkakde100@gmail.com>
2022-02-15 22:22:08 +0530

    practice_Set_1

--- first.py
+++ first.py
@@ -1,3 +1,11 @@
 import os
+from platform import python_branch
+from tkinter import W
+from wave import Wave_write
 
 print("Hello all")
+
+'''Multi Line Comments in python
+Multi Line Comments in python
+Multi Line Comments in python
+Multi Line Comments in python'''
---
+++ practiceSet_1.py
@@ -0,0 +1,44 @@
+# Problem 1
+'''Write a program to write a Twinkle Twinkle little Star Poem using a Python Program'''
+
+
+import os
+from playsound import playsound
+print(''' Twinkle twinkle little star.
+How I wonder what you are.
+Up above the world so high.
+Like a diamond in the sky.
+Twinkle twinkle little star.
+How I wonder what you are.''')
+
+
+# Problem 2
+
+# Using a REPL Print the table of 5 using it
+print(5*1)
+print(5*2)
+print(5*3)
+print(5*4)
+print(5*5)
+print(5*6)
+print(5*7)
+print(5*8)
+print(5*9)
+print(5*10)
+
+
+print('Song is playing now....')
+# Problem 3
+
+# Install a external  module and use it to perform a operation of your interest
+# playsound('C:\\Users\\user\\Downloads\\everything-feels-new-15241.mp3')
+
+# Problem -3
+
+# Write a python program to print the content of directory using os module,search online for the function which does that
+
+
+print(os.listdir())
+
+
+# CTRL+ / (forward Slash is used to make multiline comments in python )
