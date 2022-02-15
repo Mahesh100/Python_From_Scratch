b6cf56bf54d93a5b93c2e18098613986ed741040
Mahesh Kakde <maheshkakde100@gmail.com>
2022-02-15 23:29:59 +0530

    Varibles and Datatypes in Python

---
+++ 01_varibles_DataTypes.py
@@ -0,0 +1,44 @@
+
+
+from logging import StringTemplateStyle
+import numbers
+from platform import python_branch
+
+
+a = "Mahesh"   # String literal in python
+b = 345         # integer value in python
+c = 45.5        # floting point  value in python
+
+# A varible is the  name given to memory location in python
+
+# Keywords are the reserved words in python
+
+# List of keywords in python
+
+False await else import pass
+None break except in raise
+True class finally is return
+and continue for lambda try
+as def from nonlocal while
+assert del global not with
+async elif if or yield
+
+
+# Data types in python
+
+'''
+
+There are main 5 types of Data types in python
+
+1) Integers
+
+2) Floating point numbers
+
+3) String   -It is collection of charachters enclosed in Single quote or Double quote
+
+4) Booleans
+
+5) None
+
+
+'''
