a028b4e06b5337bf5416af64b48da71e8737d80f
Mahesh <maheshkakde100@gmail.com>
2022-02-26 17:12:03 +0530

    Typecasting in python

--- Operators.py
+++ Operators.py
@@ -20,12 +20,39 @@ print(a)
 
 #Comparsion Operator 
 
+#Comparsion Operator always Return Boolean value i.e Ture or Flase.
+
 b = (14>7)
 
 print(b)
 
 c = (4<3)
-print(c)
+
+
+print("The Statement is:", c)
+
+# Equal to Equal (==) Opeator in python compares the two values
+d= (4==9)
+
+e = (14!=7)
+
+print("The value of d is :", d)
+
+print("The value of e is :", e)
+
+#Logical Operators in Python
+
+bool1 = True
+bool2 = False
+
+print("The value of bool1 and bool2 ", (bool1 and bool2))
+
+print("The value of not bool2 is", (not bool2))
+
+
+
+
+
 
 
 
---
+++ typecasting.py
@@ -0,0 +1,13 @@
+#Typecasting and Type function in Python
+
+#Type casting is the way to convert one data type to another
+
+a ="3434"
+a=int(a)
+print(a)
+
+print(type(a))
+
+print(a+5)
+
+#A number can be converted into a String and String into a Number (if possible)
