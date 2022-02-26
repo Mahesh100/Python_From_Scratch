95876dd654a70d8a9e259ecbfebf1f2707531759
Mahesh <maheshkakde100@gmail.com>
2022-02-26 23:22:20 +0530

    Input function in python

---
+++ input_function.py
@@ -0,0 +1,9 @@
+# The input() function allows users to take 
+ #  input as a String from the keyboard
+
+a = input("Enter your name :- ")
+
+a = int(a)  # Convert variable into a integer if possible
+print(a)
+
+print(type(a))
