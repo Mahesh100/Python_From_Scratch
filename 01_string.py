fd7c92990472d418dec713649ba8739f447e220e
Mahesh <maheshkakde100@gmail.com>
2022-03-02 17:56:17 +0530

    01_strings

---
+++ 01_string.py
@@ -0,0 +1,13 @@
+#String in python are the sequence of Charachers that are 
+# enclosed in quotes
+
+# In python we can create Single quoted, and Multi quoted strings
+
+
+a = 34
+b ="Mahesh"
+print(a,b)
+
+print(type(a))
+
+print(type(b))
