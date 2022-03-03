fdf330ccd5e6dcc5edf25c6d263db4e0d2b3a4fa
Mahesh <maheshkakde100@gmail.com>
2022-03-03 22:48:50 +0530

    01_strings

--- 01_string.py
+++ 01_string.py
@@ -13,3 +13,36 @@ print(a,b)
 print(type(a))
 
 print(type(b))
+
+
+# Slicing of String in Python
+# Meaning of slice is as to make parts or divide in parts
+
+name = "Mahesh"   #My Name is Mahesh
+
+print(name[0:3])
+
+print(name[1:4])
+
+print(name [:4])  #It is same as name[0:4]
+
+print(name[1:])  # It is same as name[1:5]
+
+# Negative Indices in python
+
+# Negative indices always starts from -1 and it work in a reverse way
+
+print(name[-4:-1])
+
+# Slicing with the skip value in python
+
+#We can provide a skip value 
+name1 = "MaheshIsCool"
+print("This is prvious value : ", name1[0:13])
+
+d = name1[0::2]  # Every 2nd character in string is skiped
+
+d = name1[0: : 3] # Every 3rd character in string is skiped
+
+print("This is new value : ", d)
+
