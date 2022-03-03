4a9cb664467b8f4075d399071a9059e0405d4991
Mahesh <maheshkakde100@gmail.com>
2022-03-04 02:35:21 +0530

    03_functions in strings

---
+++ 03_String_functions.py
@@ -0,0 +1,25 @@
+story = " There is boy called Mahesh who is smart and hardworking having a great sense of humor"
+
+print(story)
+
+
+# length() function returns the length of string
+
+print("The length of string is : ",len(story))
+
+print(type(story))
+
+# string.endswith("...") function returns whether the varibale string ends with the 
+  # assaign argunent/suffix or not . 
+
+  #This function returns the boolean value as true or false.
+
+print(story.endswith("abcd")) # returns false value
+
+print(story.endswith("humor"))  #Reurns true as output
+
+# string.count() this function returns the count value of character in a string
+
+print("The count is :", story.count("a"))
+
+
