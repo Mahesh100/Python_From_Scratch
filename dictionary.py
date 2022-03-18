036c749fc8f3e9a1f9dab20b3c234446115f9cef
Mahesh Kakde <maheshkakde100@gmail.com>
2022-03-18 16:11:43 +0530

    Dictionary In Python

---
+++ dictionary.py
@@ -0,0 +1,26 @@
+# Dictionary In Python
+
+'''
+Python Dictionary is the unordered colliction of items
+Each iteam of the dictionary has a key/value pair
+
+Dictionaries are optimized to retrive values
+when the key is known
+
+
+
+ Creating a Python Dictionary
+
+In order to create the dictionary in python we need 
+Place the items inside curly braces {} seperated by commas.
+
+An item has a key and corrosponding value and is 
+expressed as a (key:value).
+
+While value can be any data type and can be repeat
+key must be immutable type(string, number or tuple with 
+immutable elements) and must be unique.
+
+
+
+'''
