d0df30364a4e8435a2bc3407116721e6aa1823b6
Mahesh Kakde <maheshkakde100@gmail.com>
2022-03-24 22:53:39 +0530

    Conditional Statement in python

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+var1 = int(input('Enter first number:'))
+
+var2 =int(input('Enter second number:'))
+
+var3 = int(input('Enter third number:'))
+
+if var3>var2:
+    print("var3 is Greater")
+else:
+    print("var3 is Lesser")
