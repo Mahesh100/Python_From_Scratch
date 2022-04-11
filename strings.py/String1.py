421ec5b264edb46c352aaf7f74931e2c5e1534a1
Mahesh Kakde <maheshkakde100@gmail.com>
2022-04-11 11:59:28 +0530

    String Practice

---
+++ strings.py/String1.py
@@ -0,0 +1,23 @@
+var1 = "Hello World"
+print(var1)
+
+var2 = 20
+
+var3 = 20.3
+
+var4 = "Mahesh"
+
+print(var2)
+
+print(var3)
+
+print(type(var1),type(var2))
+
+#print(var1+var3)
+
+#print(var1+var2)
+
+print(var1 +" "+ var4)
+
+
+print("Type is",type(str(var3)))
